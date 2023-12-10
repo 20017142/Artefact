1bd4a97ee622c0231951b94f580010c60f35cee6
20017142 <150794193+20017142@users.noreply.github.com>
2023-12-10 17:25:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -336,11 +336,14 @@ def update():
         return render_template ("cart.html",subTotal=subTotal, shoppingCart=shoppingCart, shopLen=shopLen, total=total, totItems=totItems, display=display, session=session )
 
 
-if __name__ == '__main__':
-    try:
+#if __name__ == '__main__':
+   # try:
         # Try to set the desired locale
-        locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
-    except locale.Error:
+      #  locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+  #  except locale.Error:
         # Handle the error by setting a default locale
-        locale.setlocale(locale.LC_ALL, 'C')
-    app.run(debug=True)
+    #    locale.setlocale(locale.LC_ALL, 'C')
+  #  app.run(debug=True)
+
+if __name__ == "__main__":
+ app.run(host='0.0.0.0', port='8080')
